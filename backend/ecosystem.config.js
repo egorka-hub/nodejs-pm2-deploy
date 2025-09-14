5e3479ddd941fb5eb361fdd5455a4125ad380cd2
Egor Nuikin <e.nuyckin@gmail.com>
2025-09-14 22:15:59 +0300

    fix: eslint

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,19 +1,19 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env.deploy") });
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env.deploy') });
 
 const {
   DEPLOY_USER,
   DEPLOY_HOST,
   DEPLOY_PATH,
   DEPLOY_REPO,
-  DEPLOY_REF = "origin/master",
+  DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
   apps: [
     {
-      name: "backend",
-      script: "./dist/app.js",
+      name: 'backend',
+      script: './dist/app.js',
     },
   ],
   deploy: {
@@ -23,8 +23,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend && scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend`,
-      "post-deploy": `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && npx pm2 restart ecosystem.config.js`,
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend && scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && npx pm2 restart ecosystem.config.js`,
     },
   },
 };
