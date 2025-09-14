ad94f23e19cb6f7d11d96602a4ed35ea5c13bd9f
Egor Nuikin <e.nuyckin@gmail.com>
2025-09-14 18:33:28 +0300

    init

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { errors } from "celebrate";
 // import cors from 'cors';
-import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
-import routes from './routes';
+import errorHandler from "./middlewares/error-handler";
+import { DB_ADDRESS } from "./config";
+import routes from "./routes";
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -18,9 +18,16 @@ mongoose.connect(DB_ADDRESS);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log('ok'));
+app.listen(PORT, () => console.log("ok"));
